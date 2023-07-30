0ed5afa10dd067b0ed712267b778a0a4602154cd
Nabell87 <nabilsabir786@gmail.com>
2023-07-30 14:27:26 +0500

    new product and user
    new page jsx and scss done for adding new product and user

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import { userInputs, productInputs } from "./formSource";
 
 function App() {
   return (
@@ -15,17 +16,17 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Home/>} />
-            <Route path="login" element={<Login/>} />
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
             <Route path="users">
-              <Route index element={<List/>} />
-              <Route path=":userId" element={<Single/>} />
-              <Route path="new" element={<New/>} />
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New inputs={userInputs} title="Add New User" />} />
             </Route>
             <Route path="products">
-              <Route index element={<List/>} />
-              <Route path=":productId" element={<Single/>} />
-              <Route path="new" element={<New/>} />
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New inputs={productInputs} title="Add New Product" />} />
             </Route>
           </Route>
         </Routes>
